bd57eac644db39187d3881d43f80e2266e28502f
jemus42 <jemus42@quantenbrot.de>
2015-12-16 15:15:09 +0100

    Add basic tadaa_aov

---
+++ R/aov.R
@@ -0,0 +1,27 @@
+#' Tadaa, anova!
+#'
+#' @param formula Formula for model, passed to \code{aov}.
+#' @param data Data for model.
+#' @param show_effect_size If \code{TRUE} (default), effect size is appended as a column.
+#' @param print Print method. Passed to \link[pixiedust]{sprinkle_print_method}
+#'
+#' @return A \code{data.frame} that's also a \code{dust} object, or depending on \code{print}.
+#' @export
+#' @examples
+#' tadaa_aov(stunzahl ~ jahrgang, data = qmtut::ngo)
+tadaa_aov <- function(formula, data = NULL, show_effect_size = TRUE, print = "console"){
+  model <- broom::tidy(aov(formula = formula, data = data))
+
+  if (show_effect_size) {
+    resid <- dplyr::last(model$sumsq)
+    model$part.eta.sq <- model$sumsq / (resid + model$sumsq)
+  }
+
+  output <- pixiedust::dust(model)
+  output <- pixiedust::sprinkle_colnames(output, statistic = "F")
+  output <- pixiedust::sprinkle(output, col = 6, fn = quote(pixiedust::pvalString(value)))
+  output <- pixiedust::sprinkle(output, col = 3:4, round = 2)
+  output <- pixiedust::sprinkle(output, round = 4)
+
+  pixiedust::sprinkle_print_method(output, print_method = print)
+}
