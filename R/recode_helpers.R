fa967fc555fe8460e17e114e3283483ed22e7dff
jemus42 <jemus42@quantenbrot.de>
2016-01-15 23:55:59 +0100

    Use thirds instead of quarters for _likertize

--- R/recode_helpers.R
+++ R/recode_helpers.R
@@ -4,7 +4,7 @@
 #' @param classes Number of classes. Defaults to 3, can also be 5.
 #' @param method How should the classes be calculated? Defaults to \code{quantiles}, can also be
 #' \code{means} for mean and standard deviation.
-#' @return A \code{factor} with \code{classes} levels. And descriptive labels.
+#' @return An ordered \code{factor} with \code{classes} levels. And descriptive labels.
 #' @importFrom car recode
 #' @export
 #' @examples
@@ -14,17 +14,17 @@ tadaa_likertize <- function(x, classes = 3, method = "quantiles"){
 
   if (classes == 3) {
     if (method == "quantiles") {
-      quantiles <- quantile(x, (1:classes) / (classes + 1))
+      quantiles <- quantile(x, (1:2) / classes)
       recodes <- paste0("lo:", quantiles[[1]], " = 1; ",
-                        quantiles[[1]], ":", quantiles[[3]], " = 2; ",
-                        quantiles[[3]], ":hi = 3")
+                        quantiles[[1]], ":", quantiles[[2]], " = 2; ",
+                        quantiles[[2]], ":hi = 3")
     } else if (method == "meansd") {
       recodes <- paste0("lo:", mean(x) - sd(x), " = 1; ",
                         mean(x) -  sd(x), ":", mean(x) + sd(x), " = 2; ",
                         mean(x) + sd(x), ":hi = 3")
     }
       xx <- car::recode(x, recodes = recodes)
-      xx <- factor(xx, labels = c("niedrig", "mittel", "hoch"))
+      xx <- factor(xx, labels = c("niedrig", "mittel", "hoch"), ordered = TRUE)
       return(xx)
   } else if (classes == 5) {
     stop("not yet implemented")
