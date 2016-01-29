eaf6d0f12c27992767c69c9e8ea988dba4ab99fc
jemus42 <jemus42@quantenbrot.de>
2016-01-29 05:45:17 +0100

    Add mean_ci_t for ggplot's stat_summary

--- R/confint.R
+++ R/confint.R
@@ -21,3 +21,29 @@ confint_t <- function(data, alpha = 0.05, na.rm = TRUE){
 
   return(width)
 }
+
+#' Get mean and ci for data
+#'
+#' Suitable for use within ggplot's \link[ggplot]{stat_summary}.
+#'
+#' @param data A \code{data.frame}
+#' @param alpha Alpha, default is \code{0.05}
+#' @param na.rm If \code{TRUE} (default), missing values are dropped.
+#'
+#' @return A \code{data.frame} with y (mean), ymin and ymax values.
+#' @export
+#' @examples
+#' df <- data.frame(x = runif(100), y = sample(c("A", "B"), 100, TRUE))
+#' mean_ci_t(df$x)
+mean_ci_t <- function(data, alpha = 0.05, na.rm = TRUE){
+  if (!is.numeric(data)) {
+    stop("Data is not a numeric vector")
+  }
+
+  m     <- mean(data, na.rm = na.rm)
+  upper <- m + confint_t(data = data, alpha = alpha, na.rm = na.rm)
+  lower <- m + confint_t(data = data, alpha = alpha, na.rm = na.rm)
+  ret   <- data.frame(y = m, ymin = lower, ymax = upper)
+
+  return(ret)
+}
