2fb53bc563a5f21b095524db3816f3a4d83bc801
jemus42 <jemus42@quantenbrot.de>
2016-02-12 21:12:43 +0100

    Embrace the Umlaut

--- R/data.R
+++ R/data.R
@@ -1,10 +1,10 @@
 #' @name ngo
 #' @title The ngo dataset
-#' @description Sample data for teaching purposes used by K{\"a}hler (2008)
+#' @description Sample data for teaching purposes used by Kähler (2008)
 #' @docType data
 #' @keywords dataset
 #' @references
-#' K{\"a}hler, W.-M. (2008). \emph{Statistische Datenanalyse: Verfahren verstehen
+#' Kähler, W.-M. (2008). \emph{Statistische Datenanalyse: Verfahren verstehen
 #' und mit SPSS gekonnt einsetzen}. Wiesbaden: Vieweg.
 #' @usage ngo
 #' @note \code{\link[ryouready]{d.ngo}} is the source of this data, but with some recoding done.
