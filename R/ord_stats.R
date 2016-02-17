20fa12dab3e1cf6f60129a9242fd46f2fff233b9
jemus42 <jemus42@quantenbrot.de>
2016-02-17 23:29:45 +0100

    Add tadaa_ord for ordinal stats

--- R/ord_stats.R
+++ R/ord_stats.R
@@ -45,3 +45,37 @@ ord_somers_d <- function(x, y = NULL, symmetric = FALSE, reverse = FALSE){
     ryouready::ord.somers.d(x)$sd.cr
   }
 }
+
+#' Get all the ordinal stats
+#'
+#' As of now, only Gamma and Somers D are supported. But let's be honest: Everybody hates Tau.
+#' @param x Dependent variable. Alternatively a \code{table}.
+#' @param y Independent variable
+#' @param round Ho many digits should be rounded. Default is 2.
+#' @param print Print method. Passed to \link[pixiedust]{sprinkle_print_method} as of now.
+#' @return A \code{dust} object, depending on \code{print}.
+#' @export
+#' @import pixiedust
+#' @family Tadaa-functions
+#' @examples
+#' tadaa_ord(ngo$abschalt, ngo$geschl)
+tadaa_ord <- function(x, y = NULL, round = 2, print = "console"){
+  if (!is.table(x)) {
+    x <- table(x, y)
+  }
+  gamma  <- round(ord_gamma(x), round)
+
+  somer_x <- round(ord_somers_d(x), round)
+  somer_y <- round(ord_somers_d(x, reverse = T), round)
+  somer_s <- round(ord_somers_d(x, symmetric = T), round)
+
+  ret <- data.frame("gamma" = gamma, "somer_x" = somer_x, "somer_y" = somer_y,
+                    "somer_s" = somer_s)
+
+  retprint <- pixiedust::sprinkle_colnames(pixiedust::dust(ret),
+                                           gamma = "Gamma",
+                                           somer_x = "Somers' D (x dep.)",
+                                           somer_y = "Somers' D (y dep.)",
+                                           somer_s = "Somers' D (sym.)")
+  return(pixiedust::sprinkle_print_method(retprint, print))
+}
