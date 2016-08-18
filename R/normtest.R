daefd748c649a9b75cc3ed54c90aa49180f12563
jemus42 <jemus42@quantenbrot.de>
2016-08-18 15:46:56 +0200

    Add tadaa_normtest, closes #3

---
+++ R/normtest.R
@@ -0,0 +1,62 @@
+#' Tadaa, test for normality!
+#'
+#' @param data A \code{data.frame}.
+#' @param method The type of test to perform. Either \code{ad} for Anderson Darling,
+#' \code{shapiro} for Shapiro-Wilk or \code{pearson} for Pearson's chi-square test.
+#' @inheritParams tadaa_aov
+#' @return A \code{data.frame} by default, otherwise \code{dust} object, depending on \code{print}.
+#' @import pixiedust
+#' @import nortest
+#' @import stats
+#' @importFrom broom tidy
+#' @importFrom nortest ad.test
+#' @importFrom nortest pearson.test
+#' @importFrom dplyr bind_rows
+#' @family Tadaa-functions
+#' @export
+#' @examples
+#' \dontrun{
+#' library(dplyr)
+#' ngo %>%
+#' select(englisch, deutsch, mathe) %>%
+#' tadaa_normtest(method = "shapiro")
+#'
+#' ngo %>%
+#' select(englisch, deutsch, mathe) %>%
+#' tadaa_normtest(method = "pearson", print = "console")
+#' }
+tadaa_normtest <- function(data, method = "ad", print = "df"){
+  vars <- names(data)
+  results <- lapply(data, function(x){
+
+    res <- if (method == "as") {
+      res <- ad.test(x)
+    } else if (method == "shapiro") {
+      res <- shapiro.test(x)
+    } else if (method == "pearson") {
+      res <- pearson.test(x)
+    } else {
+      stop("Method must be one of: 'as', 'shapiro', 'pearson'")
+    }
+
+    res <- tidy(res)
+    return(res)
+  })
+  results <- dplyr::bind_rows(results)
+  results$variable <- vars
+  results <- results[c(4,1,2,3)]
+
+  if (print == "df") {
+    return(results)
+  } else {
+    output <- pixiedust::dust(results)
+    output <- pixiedust::sprinkle(output, col = 3, fn = quote(pval_string(value)))
+    output <- pixiedust::sprinkle(output, round = 3)
+
+    if (!(print %in% c("df", "console", "html", "markdown"))) {
+      stop("Print method must be 'df', 'console', 'html' or, 'markdown'")
+    }
+
+    return(pixiedust::sprinkle_print_method(output, print_method = print))
+  }
+}
