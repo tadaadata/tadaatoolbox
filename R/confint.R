a1bd6b91bf1cf91af1e0d561bc8eb9a7b51e60b9
jemus42 <jemus42@quantenbrot.de>
2016-08-24 23:55:00 +0200

    Finalize #8, needs review

--- R/confint.R
+++ R/confint.R
@@ -47,3 +47,28 @@ mean_ci_t <- function(data, alpha = 0.05, na.rm = TRUE){
 
   return(ret)
 }
+
+#' Confidence Intervals
+#'
+#' @param data A \code{data.frame}
+#' @param alpha Alpha, default is \code{0.05}
+#' @param na.rm If \code{TRUE} (default), missing values are dropped.
+#' @return \code{numeric} of length one (size of CI in one direction)
+#' @export
+#' @import stats
+#' @examples
+#' df <- data.frame(x = runif(100), y = sample(c("A", "B"), 100, TRUE))
+#' confint_norm(df$x)
+confint_norm <- function(data, alpha = 0.05, na.rm = TRUE){
+  if (!is.numeric(data)) {
+    stop("Data is not a numeric vector")
+  }
+
+  alpha    <- alpha/2
+  sd       <- sd(data, na.rm = na.rm)
+  sem      <- sd/sqrt(length(data))
+  w_krit   <- qnorm(1 - (alpha), mean = mean(data), sd = sem)
+  width    <- w_krit * sem
+
+  return(width)
+}
--- R/tadaa_stats.R
+++ R/tadaa_stats.R
@@ -226,7 +226,7 @@ tadaa_normtest <- function(data, method = "ad", print = "df", ...){
 #' @examples
 #' df <- data.frame(x = rnorm(n = 20, mean = 100, sd = 1))
 #'
-#' tadaa_one_sample(df, x, mu = 105, sigma = 1)
+#' tadaa_one_sample(df, x, mu = 101, sigma = 1)
 #'
 #' # No data.frame, just a vector
 #' tadaa_one_sample(x = rnorm(20), mu = 0)
@@ -242,22 +242,39 @@ tadaa_one_sample <- function(data = NULL, x, mu, sigma = NULL, direction = "two.
     stop("Argument 'x' must be numeric or a bare column name of 'data'")
   }
 
+  mean_x <- mean(x, na.rm = na.rm)
+
   if (is.null(sigma)) {
     # If sigma is unknown, just do a t-test
     results <- broom::tidy(t.test(x = x, mu = mu, direction = direction))
     # Effect size
-    results$d <- (mean(x, na.rm = na.rm) - mu) / sd(x)
+    results$d <- (mean_x - mu) / sd(x)
     # Power
     results$power <- pwr::pwr.t.test(n = length(x), d = results$d, type = "one.sample",
                                      alternative = direction)$power
   } else {
     # If sigma is known, do manual z-test stuff
     sem     <- sigma/sqrt(length(x))
-    results <- data.frame(estimate  = mean(x, na.rm = na.rm),
-                          statistic = (mean(x, na.rm = na.rm) - mu)/sem)
+    results <- data.frame(estimate  = mean_x,
+                          statistic = (mean_x - mu)/sem)
+
+    if (direction == "two.sided") {
+      p <- 2 * pnorm(mean_x, mean = mu, sd = sem)
+    } else if (direction == "less") {
+      p <- pnorm(mean_x, mean = mu, sd = sem, lower.tail = TRUE)
+    } else if (direction == "greater") {
+      p <- pnorm(mean_x, mean = mu, sd = sem, lower.tail = FALSE)
+    } else {
+      stop("'direction' must be one of: 'two.sided', 'less', 'greater'")
+    }
+    results$p.value     <- p
+    results$conf.low    <- mean_x - confint_norm(x)
+    results$conf.high   <- mean_x - confint_norm(x)
+    results$method      <- "z-test"
+    results$alternative <- direction
 
     # Effect size
-    results$d <- (mean(x, na.rm = na.rm) - mu) / sigma
+    results$d <- (mean_x - mu) / sigma
     # Power
     results$power <- pwr::pwr.norm.test(d = results$d, n = length(x),
                                         alternative = direction)$power
