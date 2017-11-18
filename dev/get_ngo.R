bc0f021b16dc164e0d83396eac9edac93846238e
jemus42 <jemus42@quantenbrot.de>
2017-11-18 01:27:44 +0100

    Remove recoded leist var form ngo

--- dev/get_ngo.R
+++ dev/get_ngo.R
@@ -1,29 +1,30 @@
 #### Cleaning up ngo ####
 ngo <- ryouready::d.ngo
 
-ngo <- dplyr::select(ngo, -index, -zeng, -zdeutsch, -zmathe)
+ngo <- dplyr::select(ngo, -index, -zeng, -zdeutsch, -zmathe, -leist)
 
 ## factors
 ngo$geschl   <- factor(ngo$geschl,   labels = c("M\u00e4nnlich", "Weiblich"))
 ngo$jahrgang <- factor(ngo$jahrgang, labels = c("11", "12", "13"), ordered = TRUE)
-ngo$hausauf  <- car::recode(ngo$hausauf,  "0 = NA")
-ngo$abschalt <- car::recode(ngo$abschalt, "0 = NA")
+ngo$hausauf  <- ifelse(ngo$hausauf  == 0, NA, ngo$hausauf)
+ngo$abschalt <- ifelse(ngo$abschalt == 0, NA, ngo$abschalt)
 ngo$abschalt <- factor(ngo$abschalt, labels = c("Ja", "Nein"))
 
 ## sjPlot value labels
-ngo$geschl   <- sjmisc::set_labels(ngo$geschl,   c("M\u00e4nnlich", "Weiblich"))
-ngo$abschalt <- sjmisc::set_labels(ngo$abschalt, c("Ja", "Nein"))
-ngo$jahrgang <- sjmisc::set_labels(ngo$jahrgang, c("11", "12", "13"))
-ngo$hausauf  <- sjmisc::set_labels(ngo$hausauf,  c("gar nicht", "weniger als halbe Stunde",
-                                                   "halbe Stunde bis Stunde", "1 bis 2 Stunden",
-                                                   "2 bis 3 Stunden", "3 bis 4 Stunden",
-                                                   "mehr als 4 Stunden"))
+ngo$geschl   <- sjlabelled::set_labels(ngo$geschl,   labels = c("M\u00e4nnlich", "Weiblich"))
+ngo$abschalt <- sjlabelled::set_labels(ngo$abschalt, labels = c("Ja", "Nein"))
+ngo$jahrgang <- sjlabelled::set_labels(ngo$jahrgang, labels = c("11", "12", "13"))
+ngo$hausauf  <- sjlabelled::set_labels(ngo$hausauf,
+                                       labels = c("gar nicht", "weniger als halbe Stunde",
+                                                  "halbe Stunde bis Stunde", "1 bis 2 Stunden",
+                                                  "2 bis 3 Stunden", "3 bis 4 Stunden",
+                                                  "mehr als 4 Stunden"))
 
 ## Variable labels
-ngo$geschl   <- sjmisc::set_label(ngo$geschl, "Geschlecht")
-ngo$abschalt <- sjmisc::set_label(ngo$abschalt, "Abschalten")
-ngo$jahrgang <- sjmisc::set_label(ngo$jahrgang, "Jahrgang")
-ngo$hausauf  <- sjmisc::set_label(ngo$hausauf, "Hausaufgaben")
+ngo$geschl   <- sjlabelled::set_label(ngo$geschl,   "Geschlecht")
+ngo$abschalt <- sjlabelled::set_label(ngo$abschalt, "Abschalten")
+ngo$jahrgang <- sjlabelled::set_label(ngo$jahrgang, "Jahrgang")
+ngo$hausauf  <- sjlabelled::set_label(ngo$hausauf,  "Hausaufgaben")
 
 ## Saving
 ngo <- tibble::as_tibble(ngo)
