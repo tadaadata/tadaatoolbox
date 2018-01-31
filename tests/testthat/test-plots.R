9e1cc306b1d815f1dea149facfa26c98276e1710
jemus42 <jemus42@quantenbrot.de>
2018-01-31 05:05:10 +0100

    More plot coverage

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -1,19 +1,21 @@
-context("test-plots.R")
+context("Tadaa, Plots!")
 
 test_that("tadaa_int produces plot", {
   expect_is(tadaa_int(ngo, stunzahl, jahrgang, geschl, grid = FALSE, print = FALSE), "list")
+  expect_is(tadaa_int(ngo, stunzahl, jahrgang, geschl, grid = FALSE, show_n = TRUE, print = FALSE)[[1]], "gg")
   expect_is(tadaa_int(ngo, stunzahl, jahrgang, geschl, grid = FALSE, print = FALSE)[[1]], "gg")
   expect_is(tadaa_int(ngo, stunzahl, jahrgang, geschl, grid = FALSE, print = FALSE)[[2]], "gg")
   expect_length(tadaa_int(ngo, stunzahl, jahrgang, geschl, grid = TRUE, print = FALSE), 2)
 
   if (requireNamespace("cowplot")) {
     expect_is(tadaa_int(ngo, stunzahl, jahrgang, geschl, grid = TRUE, print = FALSE)[[1]], "gg")
-    expect_length(tadaa_int(ngo, stunzahl, jahrgang, geschl, grid = FALSE, print = FALSE), 2)
+    expect_length(tadaa_int(ngo, stunzahl, jahrgang, geschl, grid = FALSE, print = TRUE), 2)
   }
 })
 
 test_that("tadaa_balance produces plot", {
   expect_is(tadaa_balance(ngo, jahrgang, geschl), "gg")
+  expect_is(tadaa_balance(ngo, jahrgang, geschl, annotate = TRUE), "gg")
 })
 
 test_that("tadaa_mean_ci procudes plot", {
