ea328490ec00d809ff7c60b74c79f1b776668cad
Lukas Burk <lukas@quantenbrot.de>
2018-11-21 16:38:18 +0100

    inv: Add example, fix typo

--- R/invert_scales.R
+++ R/invert_scales.R
@@ -1,11 +1,12 @@
 #' Inverting scales
 #'
-#' @param x A vector of numric data.
-#' @param min the minimum value of the scale.
-#' @param max the maximum value of the scale.
-
-
-
+#' @param x A vector of numeric data.
+#' @param min The minimum value of the scale.
+#' @param max The maximum value of the scale.
+#'
+#' # Assuming you have a Likert-scale from 1 to 9
+#' x <- c(4, 5, 2, 3, 7, 8, 3)
+#' inv(x, 1, 9)
 inv <- function(x, min, max) {
 
   range <- max + min
