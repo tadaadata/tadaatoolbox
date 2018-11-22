df86e306fbafc797035d6d3a34a8468f0cb17572
Lukas Burk <lukas@quantenbrot.de>
2018-11-22 14:56:26 +0100

    Add export, example tag (that's kind of important)

--- R/invert_scales.R
+++ R/invert_scales.R
@@ -3,7 +3,8 @@
 #' @param x A vector of numeric data.
 #' @param min The minimum value of the scale.
 #' @param max The maximum value of the scale.
-#'
+#' @export
+#' @examples
 #' # Assuming you have a Likert-scale from 1 to 9
 #' x <- c(4, 5, 2, 3, 7, 8, 3)
 #' inv(x, 1, 9)
